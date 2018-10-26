113e0af8fd2aba1e96f4864895efb2080fa0d150
Akshita <31439578+akshusharma1526@users.noreply.github.com>
2018-10-26 17:45:45 +0530

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+
+int main() {
+    int A[1000],N,i;
+    scanf("%d",&N);
+    for(i=0;i<N;i++)
+    {
+    scanf("%d",&A[N]);
+    }
+    for(i=N-1;i>=0;i--)
+    {
+        printf("%d", A[N]);
+    }
+    
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
+
